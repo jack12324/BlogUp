efcb9d2bc9dcf34ab89109a69f22b00abca55fe9
Jack <jacksporter24@gmail.com>
2023-06-08 13:12:21 -0400

    Deployed app with static frontend
    Preparing scripts for pipeline

--- backend/app.js
+++ backend/app.js
@@ -24,6 +24,8 @@ mongoose
     logger.error("error connecting to MongoDB", error.message);
   });
 
+app.use(express.static("build"));
+
 app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
